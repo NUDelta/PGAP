730105706fc36a3e69d3138fe1827e1ee269f40f
Shawn Caeiro <shawncaeiro@gmail.com>
2015-10-12 19:44:19 -0500

    Initial Commit

---
+++ PGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PGames
+//
+//  Created by Shawn Caeiro on 10/12/15.
+//  Copyright © 2015 Shawn Caeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PGamesTests/PGamesTests.swift
@@ -0,0 +1,36 @@
+//
+//  PGamesTests.swift
+//  PGamesTests
+//
+//  Created by Shawn Caeiro on 10/12/15.
+//  Copyright © 2015 Shawn Caeiro. All rights reserved.
+//
+
+import XCTest
+@testable import PGames
+
+class PGamesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
