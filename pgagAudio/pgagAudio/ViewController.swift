d2d6f4cc765d7c9c64fbed66c38efffaefabfa99
Shawn Caeiro <shawncaeiro@gmail.com>
2016-04-08 14:37:58 -0500

    Play Game Function

--- pgagAudio/pgagAudio/ViewController.swift
+++ pgagAudio/pgagAudio/ViewController.swift
@@ -8,18 +8,109 @@
 
 import UIKit
 import Parse
+import AVFoundation
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    var locationManager: CLLocationManager!
+    var currLocation: CLLocation!
+    enum GameStatus {
+        case preintro
+        case playing
+        case looking
+        case postconclusion
+    }
+    var currGameStatus = GameStatus.preintro
+
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        // Create Location Manage
+        locationManager = CLLocationManager();
+        self.locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print(manager.location?.coordinate)
+        currLocation = (manager.location)!
+        var gamesPlayed:[String] = []
+        
+        // TODO Save location to DB
+        
+        if currGameStatus == .looking {
+            let objects = getObjects(currLocation)
+            let affordances = getAffordances(objects)
+            let game = getGame(affordances, gamesPlayed: gamesPlayed)
+            if (game != nil) {
+                playGame(game!)
+            }
+        }
+    }
+    
+    func playGame(game: (title: String, task: String, conclusion: String, duration: Int, obj: String)) {
+        
+        let synth = AVSpeechSynthesizer()
+        synth.pauseSpeakingAtBoundary(.Word)
+        
+        let alertPlayer = makeAudioPlayer("beep", type: "wav")
+        alertPlayer.play()
+        
+        while alertPlayer.playing {
+            // Do Nothing
+        }
+        
+        let task_speech = makeSpeechUtterance(game.task)
+        synth.speakUtterance(task_speech)
+        
+        while synth.speaking {
+            // Do Nothing
+        }
+        
+        // #HACK
+        sleep(UInt32(game.duration))
+        let conclusion_speech = makeSpeechUtterance(game.conclusion)
+        synth.speakUtterance(conclusion_speech)
+
+        
+    }
+    
+    func makeAudioPlayer(file: String, type: String) -> AVAudioPlayer {
+        
+        //define file path
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        var audioPlayer:AVAudioPlayer!
+        
+        //create the player with the specific audio file
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not available")
+        }
+        
+        return audioPlayer
+    }
+    
+    func makeSpeechUtterance(speech: String) -> AVSpeechUtterance {
+        let game_speech = AVSpeechUtterance(string: speech)
+        game_speech.rate = 0.5
+        game_speech.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        game_speech.pitchMultiplier = 1.5
+        return game_speech
     }
     
     func getObjects(loc: CLLocation) -> [String] {
@@ -62,8 +153,8 @@ class ViewController: UIViewController {
         return affordance_objs
     }
     
-    func getGames(affordances: [(affordance: String, obj: String)], var gamesPlayed: [String])
-        -> (title: String, game: String, conclusion: String, duration: Int, obj: String)? {
+    func getGame(affordances: [(affordance: String, obj: String)], var gamesPlayed: [String])
+        -> (title: String, task: String, conclusion: String, duration: Int, obj: String)? {
             var affordance_names:[String] = []
             for a in affordances {
                 affordance_names.append(a.affordance)
