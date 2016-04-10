6a242fac74c9be0a3280f021f5e86d5b4f390153
jenniewerner <jenniewerner2018@u.northwestern.edu>
2016-04-10 13:21:19 -0500

    new broken code, attempting to query based on affordances

--- pgagAudio/pgagAudio/ViewController.swift
+++ pgagAudio/pgagAudio/ViewController.swift
@@ -11,18 +11,24 @@ import Parse
 import AVFoundation
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDelegate {
+    //Database Names
+    
+    let OBJECT_DB = "WorldObject"   //label, location
+    let MAPPING_DB = "WorldMapping" //name, affordance
+    let GAMES_DB = "WorldTask" //title, task, conclusion, affordance duration, validated
     
     var locationManager: CLLocationManager!
-    var currLocation: CLLocation!
+    var currLocation: CLLocation?
     enum GameStatus {
         case preintro
         case playing
         case looking
         case postconclusion
     }
-    var currGameStatus = GameStatus.preintro
-
+    var currGameStatus = GameStatus.looking
+    
+    var gamesPlayed:[String] = []
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,17 +51,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //print(manager.location?.coordinate)
-        currLocation = (manager.location)!
-        var gamesPlayed:[String] = []
-        
+        currLocation = manager.location
         // TODO Save location to DB
         
         if currGameStatus == .looking {
-            let objects = getObjects(currLocation)
+            let objects = getObjects(currLocation!)
             let affordances = getAffordances(objects)
+            print("about to get a game")
             let game = getGame(affordances, gamesPlayed: gamesPlayed)
+            print("we got a game yo")
             if (game != nil) {
                 playGame(game!)
+                gamesPlayed.append(game!.title)
             }
         }
     }
@@ -114,42 +121,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func getObjects(loc: CLLocation) -> [String] {
+        print("entered objects")
         var objects:[String] = []
         var objects_nearby:[PFObject] = []
         
-        let query = PFQuery(className: "OBJ_DB")
+        let query = PFQuery(className: OBJECT_DB)
         let user_loc = PFGeoPoint(location:loc)
         query.whereKey("location", nearGeoPoint: user_loc, withinMiles: 0.01)
         do {
             try objects_nearby = query.findObjects()
             for obj in objects_nearby {
-                let name = obj["name"] as! (String)
+                let name = obj["label"] as! (String)
                 if !objects.contains(name) {
                     objects.append(name)
                 }
             }
         } catch {}
+        print("return")
         return objects
     }
     
     func getAffordances(objects: [String]) -> [(affordance: String, obj: String)] {
+        print("1")
         var affordance_objs:[(affordance: String, obj: String)] = []
         var affordance_names:[String] = []
         
-        let query = PFQuery(className: "MAPPING_DB")
+        let query = PFQuery(className: MAPPING_DB)
         query.whereKey("objectName", containedIn: objects)
         do {
+            print("2")
             var obj_affordances:[PFObject] = []
             try obj_affordances = query.findObjects()
             // Not necessarily in order of closest affordances!!!!!!!!!
             for a in obj_affordances {
+                print("3")
                 if !(affordance_names.contains(a["affordance"] as! String)){
                     affordance_objs.append((affordance: a["affordance"] as! String, obj: a["name"] as! String))
                     affordance_names.append(a["affordance"] as! (String))
                 }
             }
         } catch {}
-        
+        print("5")
         return affordance_objs
     }
     
@@ -160,27 +172,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 affordance_names.append(a.affordance)
             }
             
-            let query = PFQuery(className: "GAMES_DB")
+            let query = PFQuery(className: GAMES_DB)
             query.whereKey("affordance", containedIn: affordance_names)
+            query.whereKey("titles", notContainedIn: gamesPlayed)
+            query.whereKey("validated", equalTo: true)
+            query.limit = 1
+
             do {
                 var games_possible:[PFObject] = []
                 try games_possible = query.findObjects()
                 // Not in order
-                for g in games_possible {
-                    let title = g["title"] as! String
-                    if !gamesPlayed.contains(title){
-                        gamesPlayed.append(title)
-                        // TODO - How to use gamesPlayed... is it Global or do array references work as intended?
-                        var obj = ""
-                        for a in affordances {
-                            if a.affordance == g["affordance"] as! String {
-                                obj = a.obj
-                            }
-                        }
-                        return (title, g["game"] as! String, g["conclusion"] as! String, g["duration"] as! Int, obj)
+                    // TODO - How to use gamesPlayed... is it Global or do array references work as intended?
+                let g = games_possible[0]
+                var obj = ""
+                
+                for a in affordances {
+                    if a.affordance == g["affordance"] as! String {
+                        obj = a.obj
                     }
                 }
+                
+                return (g["title"] as! String, g["task"] as! String, g["conclusion"] as! String, g["duration"] as! Int, obj)
+                
             } catch {}
+            
             return nil
     }
     /*
